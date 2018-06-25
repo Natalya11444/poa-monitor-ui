12305e3b54b17ab984ba9d7d71b655bd01fd9ae9
Natalia <natalia.gulko@yahoo.com>
2018-06-25 12:15:33 -0700

    Added component for failed txs in the sending txs test

--- src/components/txs-sending/MissingTxsRun.js
+++ src/components/txs-sending/MissingTxsRun.js
@@ -2,6 +2,7 @@ import React from "react";
 import ArrayElement from "../ArrayElement"
 import passedImg from '../../img/passed-icon.png';
 import failedImg from '../../img/failed-icon.png';
+import FailedTxElement from "./FailedTxElement";
 
 function MissingTxsRun(props) {
     let iconSrc = props.run.passed === 1 ? passedImg : failedImg;
@@ -16,7 +17,7 @@ function MissingTxsRun(props) {
                 }
             </td>
             <td>
-                {props.run.failedTxs.map(t => <ArrayElement element={t}/>)
+                {props.run.failedTxs.map(t => <FailedTxElement tx={t}/>)
                 }
             </td>
         </tr>
